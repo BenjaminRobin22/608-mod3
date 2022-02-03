8305ea766ee85894dc23b5296a9ab05dec648f16
BenjaminRobin22 <97648818+BenjaminRobin22@users.noreply.github.com>
2022-02-03 16:57:19 -0600

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -1,9 +1,5 @@
-#this is Ben Robin's calculator
+# This is Ben Robin's Calculator (3)
 
 import math
-print("This is the square root of 900")
-math.sqrt(900)
-print("This is the absolute value of -10")
-math.fabs(-10)
-
-# floor and ceil would definitly be useful in my line of work we round constantly and often have to only round up or down
+print("This is the square root of 900:",math.sqrt(900))
+print("This is the absolute value of -10:",math.fabs(-10))
