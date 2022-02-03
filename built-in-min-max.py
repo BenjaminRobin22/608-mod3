65e4051760cae17e94dbfd1385e42fe598fbb256
BenjaminRobin22 <97648818+BenjaminRobin22@users.noreply.github.com>
2022-02-02 21:46:02 -0600

    Create built-in-min-max.py

---
+++ built-in-min-max.py
@@ -0,0 +1,15 @@
+# This is Ben Robin's 3rd project 
+
+# built-in-min-max 1
+max(12,27,26)
+
+#First max value 1 
+max(15,77,85,85.5)
+
+#second max value 1
+min(15,9,27,14)
+
+# first min value 1
+min(2,4,6.5,-1)
+
+print("confimred values are corect")
